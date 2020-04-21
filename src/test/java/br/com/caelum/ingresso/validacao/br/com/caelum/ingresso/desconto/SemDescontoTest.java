cd3344f938dfffb00cacd50f10933d980a2442ec
Alefh Sousa <alefh.sousa@gmail.com>
2020-04-20 22:53:12 -0300

    adicionando modulo de desconto ao projeto

---
+++ src/test/java/br/com/caelum/ingresso/validacao/br/com/caelum/ingresso/desconto/SemDescontoTest.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ingresso.validacao.br.com.caelum.ingresso.desconto;
+
+
+import br.com.caelum.ingresso.desconto.SemDesconto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class SemDescontoTest {
+
+    @Test
+    public void naoDeveAplicarDesconto() {
+        SemDesconto semDesconto = new SemDesconto();
+        BigDecimal vinteReais = new BigDecimal("20.00");
+        BigDecimal valorComDescontoAplicado = semDesconto.calculaDesconto(vinteReais);
+
+        Assert.assertEquals(vinteReais, valorComDescontoAplicado);
+    }
+}
